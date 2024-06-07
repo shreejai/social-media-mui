7b10c8e210b94d8e0a124be80dd699ec51d3eafc
Shreejai Raj <shreejairaj@gmail.com>
2024-06-07 17:48:59 +1000

    dropdown menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
-import { AppBar, Avatar, Badge, Box, Icon, InputBase, Toolbar, Typography, styled } from '@mui/material'
-import React from 'react'
+import { AppBar, Avatar, Badge, Box, Icon, InputBase, Menu, MenuItem, Toolbar, Typography, styled } from '@mui/material'
+import React, { useState } from 'react'
 import PetsIcon from '@mui/icons-material/Pets';
 import { Mail, Notifications } from '@mui/icons-material';
 
@@ -34,6 +34,7 @@ const UserBox = styled(Box)(({theme}) =>({
 }))
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <AppBar position="sticky">
       <StyledToolbar>
@@ -49,13 +50,34 @@ const Navbar = () => {
           <Badge badgeContent={4} color="error">
             <Notifications/>
           </Badge>
-          <Avatar sx={{width:"30px", height:"30px"}} src="https://media.licdn.com/dms/image/C4E03AQHQMaH69VBmBg/profile-displayphoto-shrink_800_800/0/1517428454455?e=1723075200&v=beta&t=TT8vTm6l87lDwS-ynDfujA1Ds_Tjxwy99mFnktjbmiw"/>
+          <Avatar sx={{width:"30px", height:"30px"}} src="https://media.licdn.com/dms/image/C4E03AQHQMaH69VBmBg/profile-displayphoto-shrink_800_800/0/1517428454455?e=1723075200&v=beta&t=TT8vTm6l87lDwS-ynDfujA1Ds_Tjxwy99mFnktjbmiw"
+          onClick={(e)=>setOpen(true)}  
+          />
         </Icons>
-        <UserBox>
+        <UserBox onClick={(e)=>setOpen(true)}>
           <Avatar sx={{width:"30px", height:"30px"}} src="https://media.licdn.com/dms/image/C4E03AQHQMaH69VBmBg/profile-displayphoto-shrink_800_800/0/1517428454455?e=1723075200&v=beta&t=TT8vTm6l87lDwS-ynDfujA1Ds_Tjxwy99mFnktjbmiw"/>
           <Typography variant='span'>Jay</Typography>
         </UserBox> 
       </StyledToolbar>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        open={open}
+        onClose={(e)=>setOpen(false)} 
+        // When clicked anywhere else
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
     </AppBar>
   )
 }
